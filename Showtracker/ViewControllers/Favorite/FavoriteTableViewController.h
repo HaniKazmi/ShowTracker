9594b9c53fc479140901f5f153e7dc6d349b4cb2
Hani Kazmi <hanikazmi@me.com>
2012-04-05 17:32:34 +0100

    Release 1

---
+++ Showtracker/ViewControllers/Favorite/FavoriteTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FavoriteTableViewController.h
+//  Showtracker
+//
+//  Created by Hani Kazmi on 05/04/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FavoriteTableViewController : UITableViewController
+
+@end

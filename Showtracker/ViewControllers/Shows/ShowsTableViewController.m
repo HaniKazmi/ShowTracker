259581402c56f60fef9475be019db31081c6d01c
Hani Kazmi <hanikazmi@me.com>
2012-04-06 13:27:13 +0100

    Add Show

--- Showtracker/ViewControllers/Shows/ShowsTableViewController.m
+++ Showtracker/ViewControllers/Shows/ShowsTableViewController.m
@@ -3,7 +3,7 @@
 //  Showtracker
 //
 //  Created by Hani Kazmi on 05/04/2012.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012 Hani Kazmi. All rights reserved.
 //
 
 #import "ShowsTableViewController.h"
